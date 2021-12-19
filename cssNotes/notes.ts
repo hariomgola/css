40f81d8efe5f5aa9fc799b228b75b6f5f3ac3305
Hariom <harim987316@gmail.com>
2021-12-19 14:46:26 +0530

    code check-in

---
+++ cssNotes/notes.ts
@@ -0,0 +1,57 @@
+/**
+ 
+ # Css Selectors
+   - CSS element Selector      - p
+   - CSS id Selector           - #para1
+   - CSS class Selector        - .center (p.centre) p element with class centre
+   - CSS Universal Selector    - *
+   - CSS Grouping Selector     - h1, h2, p{}
+
+ # How to Add css
+   - External CSS              - Define as a saperate file and then import it
+   - Internal CSS              - Define with in the 'style' tag
+   - Inline CSS                - Define with in the element itself
+
+ # Css has wide range of color 
+   - check - https://www.w3schools.com/colors/colors_names.asp
+   
+ # Css background
+   - background-color: lightblue;  // selecting the background color
+   - opacity: 0.3; // changing the darkness of the color
+   - background-image: url("paper.gif"); // sets the image in the background
+   - background-repeat: repeat-x; // reapeats the background only on x portion
+   - background-repeat: no-repeat; // its not going to repeat itself
+   - background-position: right top; // puts the image into the right top position
+   - background-attachment: fixed; // Here the image will be fixed and not change on scroll
+   - background-attachment: scroll; // Here the image with move with scroll effect
+   - background: #ffffff url("img_tree.png") no-repeat right top; // also use all above property in one line
+
+ # Css Boarder
+    p.dotted {border-style: dotted;}
+    p.dashed {border-style: dashed;}
+    p.solid {border-style: solid;}
+    p.double {border-style: double;}
+    p.groove {border-style: groove;}
+    p.ridge {border-style: ridge;}
+    p.inset {border-style: inset;}
+    p.outset {border-style: outset;}
+    p.none {border-style: none;}
+    p.hidden {border-style: hidden;}
+    p.mix {border-style: dotted dashed solid double;}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
